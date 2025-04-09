f5e88d5930d129a8c7b634e1abaf8a24a1fd93bb
fionas14 <siregarfiona90@gamil.com>
2025-04-09 11:30:22 +0700

    Menambahkan Dropdown di page 2

--- app/src/main/java/com/fionasiregar0032/taxflow/screen/SecondScreen.kt
+++ app/src/main/java/com/fionasiregar0032/taxflow/screen/SecondScreen.kt
@@ -1,5 +1,6 @@
 package com.fionasiregar0032.taxflow.screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -7,26 +8,40 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SecondScreen(navController: NavHostController) {
+    var expanded by remember { mutableStateOf(false) }
+    val options = listOf("PPN", "PPh")
+    var selectOption by remember { mutableStateOf(options[0]) }
+
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
-                    Text("Hitung Pajak")
+                    Text("Hitung Pajak", fontWeight = FontWeight.Bold)
                 },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = Color(0xFF90EE90),
@@ -41,9 +56,50 @@ fun SecondScreen(navController: NavHostController) {
                 .fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text("Halaman kedua")
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+                    .padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ){
+
+
+                Text("Pilih Jenis Pajak", style = MaterialTheme.typography.titleMedium)
+
+                ExposedDropdownMenuBox(
+                    expanded = expanded,
+                    onExpandedChange = { expanded = !expanded }
+                ) {
+                    TextField(
+                        value = selectOption,
+                        onValueChange = {},
+                        readOnly = true,
+                        label = { Text("Jenis Pajak") },
+                        trailingIcon = {
+                            ExposedDropdownMenuDefaults.TrailingIcon(expanded)
+                        },
+                        modifier = Modifier.menuAnchor()
+                    )
+
+                    ExposedDropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        options.forEach { item ->
+                            DropdownMenuItem(
+                                text = { Text(item) },
+                                onClick = {
+                                    selectOption = item
+                                    expanded = false
+                                }
+                            )
+                        }
+                    }
+                }
                 Spacer(modifier = Modifier.height(24.dp))
+
                 Button(onClick = {
                     navController.popBackStack()
                 }) {
