902fd107d4bddfae64d0e074bb205af922d9e2f7
fionas14 <siregarfiona90@gamil.com>
2025-04-11 00:28:04 +0700

    Menambahkan file baru di package screen

---
+++ app/src/main/java/com/fionasiregar0032/taxflow/navigation/AboutScreen.kt
@@ -0,0 +1,97 @@
+package com.fionasiregar0032.taxflow.navigation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.fionasiregar0032.taxflow.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AboutScreen(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+            navigationIcon = {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.balik),
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+            },
+
+            title = { Text("Tentang Aplikasi") },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = Color(0xFF90EE90),
+                    titleContentColor = Color.Black
+                )
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(24.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.gambar_pajak),
+                contentDescription = "Ilustrasi Pajak",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .height(250.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(12.dp))
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "TaxFlow",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = stringResource(R.string.taxflow),
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.fillMaxWidth(),
+                lineHeight = 20.sp
+
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/fionasiregar0032/taxflow/navigation/SettingScreen.kt
@@ -0,0 +1,90 @@
+package com.fionasiregar0032.taxflow.navigation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.fionasiregar0032.taxflow.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingScreen(navController: NavHostController,
+                  onToggleTheme: () -> Unit
+                  ) {
+    var isDarkThemeEnabled by remember { mutableStateOf(false) }
+    var isNotificationEnabled by remember { mutableStateOf(true) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = {navController.popBackStack() }) {
+                        Icon(
+                            imageVector =Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.balik),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
+                title = { Text("Pengaturan") },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = Color(0xFF90EE90),
+                    titleContentColor = Color.Black
+                )
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(24.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text("Pengaturan Aplikasi", fontWeight = FontWeight.Bold, fontSize = 20.sp)
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text("Tema Gelap", modifier = Modifier.weight(1f))
+                Switch(
+                    checked = isDarkThemeEnabled,
+                    onCheckedChange = { isDarkThemeEnabled = it
+                    onToggleTheme()}
+                )
+            }
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text("Notifikasi", modifier = Modifier.weight(1f))
+                Switch(
+                    checked = isNotificationEnabled,
+                    onCheckedChange = { isNotificationEnabled = it }
+                )
+            }
+        }
+
+    }
+}
